26a6b06affa4dbb212737344b947cb8aec366d3b
Penn Su <pennsu@gmail.com>
2023-01-26 16:13:02 -0800

    Improve seed + mailing list as well

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -9,22 +9,22 @@ import { parse, parser } from 'csv'
 const prisma = new PrismaClient()
 
 // Turning stream into a promise that return an array of object parsed by parser
-function streamAsPromise(stream: parser.Parser): Promise<any[]> {
+function streamAsPromise(stream: parser.Parser): Promise<string[][]> {
     return new Promise((resolve, reject) => {
-        let data: any[] = [];        
-        stream.on("data", (row: any) => data.push(row));
+        const data: string[][] = [];        
+        stream.on("data", (row: string[]) => data.push(row));
         stream.on("end", () => resolve(data));
         stream.on("error", error => reject(error));
     });
 }
 
 // combined data of multiple fields into one address field
-function addressCombined(row: any, columns: any[]) {
+function addressCombined(row: string[], columns: string[]) {
 	const addressStreet = [
-		(row[indexOf(columns, 'MACPA_PreferredAddressLine1')] || '').trim(),
-		(row[indexOf(columns, 'MACPA_PreferredAddressLine2')] || '').trim(),
-		(row[indexOf(columns, 'MACPA_PreferredAddressLine3')] || '').trim(),
-		(row[indexOf(columns, 'MACPA_PreferredAddressLine4')] || '').trim(),
+		(row[indexOf<string>(columns, 'MACPA_PreferredAddressLine1')] || '').trim(),
+		(row[indexOf<string>(columns, 'MACPA_PreferredAddressLine2')] || '').trim(),
+		(row[indexOf<string>(columns, 'MACPA_PreferredAddressLine3')] || '').trim(),
+		(row[indexOf<string>(columns, 'MACPA_PreferredAddressLine4')] || '').trim(),
 	];
 	const location = [
 		compact(addressStreet).join(' ').trim(),
@@ -40,13 +40,13 @@ function addressCombined(row: any, columns: any[]) {
 	return combinedAddress || 'No address';
 }
 
+function readStreamForColumn(filename: string) {
+    return fs.createReadStream(path.join(__dirname, filename))
+        .pipe(parse({ delimiter: "^", from_line: 1, to_line: 1 }));
+}
 
-async function main() {
-    const columns = flatten(await streamAsPromise(fs.createReadStream(path.join(__dirname, "../data/vwPersons.csv"))
-        .pipe(parse({ delimiter: "^", from_line: 1, to_line: 1 }))));
-    
-    // persons
-    const rows = await streamAsPromise(fs.createReadStream(path.join(__dirname, "../data/vwPersons.csv"))
+function readStreamForData(filename: string) {
+    return fs.createReadStream(path.join(__dirname, filename))
         .pipe(parse({
             delimiter: "^",
             from_line: 2,
@@ -57,21 +57,29 @@ async function main() {
                     return value;
                 } 
             }
-        })));
+        }))
+}
+
+async function main() {
+    // Persons
+    const columns = flatten(
+        await streamAsPromise(readStreamForColumn("../data/vwPersons.csv")).catch(() => [])
+    );
+    const rows = await streamAsPromise(readStreamForData("../data/vwPersons.csv")).catch(() => []);
         
     // ref: https://github.com/prisma/prisma/issues/9196
     const i = pipe(rows, page(10000));
-
     for (const chunks of i) {
         // Only import alive people (look at micohort-postgresql for usable_person query)
-        const filterChunks = (rows: any[]) => {
+        const filterChunks = (rows: string[][]) => {
             return rows.filter(r => {
-                return parseInt(r[indexOf(columns, 'Status')]) != 5 && !['111403', '2464'].includes(r[indexOf(columns, 'ID')]);
+                return parseInt(r[indexOf<string>(columns, 'Status')] ?? '-1') != 5
+                    && !['111403', '2464'].includes(r[indexOf<string>(columns, 'ID')] ?? '-1');
             })
         }
-        await prisma.micpaPerson.createMany({
+        const persons = await prisma.micpaPerson.createMany({
             data: filterChunks(chunks).map(row => ({
-                id: row[indexOf(columns, 'ID')],
+                id: row[indexOf<string>(columns, 'ID')] as string,
                 name: row[indexOf(columns, 'FirstLast')] || 'No name',
                 email: row[indexOf(columns, 'Email1')] || 'No email',
                 company: row[indexOf(columns, 'NameWCompany')] || 'No company',
@@ -79,7 +87,38 @@ async function main() {
             })),
             skipDuplicates: true,
         })
-        console.log('persons chunk')
+        console.log(`${persons.count} persons imported`)
+    }
+
+    // mailing list (for testing purposes)
+    interface InputMailingList {
+        id: string;
+        title: string;
+        list: string[];
+        last_edited_at: string;
+        created_at: string;
+    }
+    const data = JSON.parse(fs.readFileSync(path.join(__dirname, "../data/mailing_list.json")).toString()) as InputMailingList[];
+
+    const mailingLists = await prisma.mailingList.createMany({
+        data: data.map(row => ({
+            id: row.id,
+            title: row.title,
+        })),
+        skipDuplicates: true,
+    });
+    console.log(`${mailingLists.count} mailing lists imported`)
+
+    for (const row of data) {
+        const listsOnPersons = await prisma.mailingListsOnPersons.createMany({
+            data: row.list.map(personId => ({
+                // id is defined automatically in schema based on the two fields below
+                mailingListId: row.id,
+                personId: personId,
+            })),
+            skipDuplicates: true,
+        });
+        console.log(`${listsOnPersons.count} lists on persons imported`)
     }
 }
 main()
